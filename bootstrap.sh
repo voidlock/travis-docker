5ab45329fcd41c809e1623d7cb3cf91237f8dc8a
Alex Arnell <alex@heroku.com>
2015-05-15 00:04:31 -0700

    print more info

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,51 +17,43 @@ bootstrap_header "Set required environment variables"
 travis_cmd export\ SLIRP_HOST\=\"\$\(/sbin/ifconfig\ venet0:0\ \|\ grep\ \'inet\ addr\'\ \|\ awk\ -F:\ \'\{print\ \$2\}\'\ \|\ awk\ \'\{print\ \$1\}\'\)\" --echo
 travis_cmd export\ SLIRP_PORTS\=\"\$\(listify\ ,\ 2375\ \$\{SLIRP_PORTS:-\$\(seq\ 49153\ 49253\)\}\)\" --echo
 travis_cmd export\ DOCKER_HOST\=\"tcp://\$\{SLIRP_HOST\}:2375\" --echo
-travis_cmd export\ DOCKER_PORT_RANGE\=\"2400:2500\" --echo
+travis_cmd export\ DOCKER_PORT_RANGE\=\"49153:49253\" --echo
 echo
+echo "SLIRP_HOST=\"${SLIRP_HOST}\""
+echo "SLIRP_PORTS=\"${SLIRP_PORTS}\""
+echo "DOCKER_HOST=\"${DOCKER_HOST}\""
+echo "DOCKER_PORT_RANGE=\"${DOCKER_PORT_RANGE}\""
 
 bootstrap_header "Disable post-install autorun"
 echo exit 101 | sudo tee /usr/sbin/policy-rc.d
 sudo chmod +x /usr/sbin/policy-rc.d
 
 
-set +x
-echo "Installing dependencies" >&2
-set -x
+bootstrap_header "Installing dependencies"
 sudo apt-get update
 sudo apt-get install -y slirp lxc aufs-tools cgroup-lite
 
 
-set +x
-echo "Installing docker" >&2
-set -x
+bootstrap_header "Installing docker"
 curl -s https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
 sudo chown -R $USER /etc/docker
 
 
 if [ -z ${NO_COMPOSE+1} ]; then
-  set +x
-  echo "Installing docker-compose" >&2
-  set -x
+  bootstrap_header "Installing docker-compose"
   sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 
-set +x
-echo "Downloading sekexe" >&2
-set -x
+bootstrap_header "Downloading sekexe"
 git clone git://github.com/voidlock/sekexe .sekexe
 
-set +x
-echo "Starting Docker deamon" >&2
-set -x
+bootstrap_header "Starting Docker deamon"
 ./.sekexe/run 'docker -d -H tcp://0.0.0.0:2375' &
 
-set +x
-echo "Waiting for Docker to come online" >&2
-set -x
+bootstrap_header "Waiting for Docker to come online"
 while ! docker info; do sleep 1; done
 
 docker version
